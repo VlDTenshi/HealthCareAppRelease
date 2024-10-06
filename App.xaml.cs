29d45176971db7b1a2a9401a721105e5d7ec2862
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2024-10-06 17:09:12 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HealthCare
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
